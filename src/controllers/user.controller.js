02a6ad165be00203e0c479eee744ca3c9d3bd190
Ishant <ishantawasthi350@gmail.com>
2025-12-11 21:50:47 +0530

    add user watch history retrieval feature

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -530,6 +530,71 @@ const registerUser = asyncHandler(async (req, res) => {
     })
 
 
+
+     const getWatchHistory= asyncHandler(async(req,res)=>{
+
+      const user= await User.aggregate([
+
+
+         {
+            $match:{
+                _id:new mongoose.Types.ObjectId(req.user?._id)
+            }
+         },
+         
+         {
+            $lookup:{
+                from:"Videos",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory",
+                pipeline:[
+                    
+                    {
+                        $lookup:{
+                            from:"Users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullname:1,
+                                        username:1,
+                                        avatar:1
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+         }
+
+      ])
+
+        
+
+
+      return res
+      .status(200)
+      .json( new ApiResponse(
+        200,
+        {watchHistory:user.watchHistory},
+            " User watch history fetched successfully"
+              ))
+
+
+
+
+
+
+
+     })
+
+
+
+
 export {
     
     registerUser,
@@ -541,5 +606,6 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory
 };
